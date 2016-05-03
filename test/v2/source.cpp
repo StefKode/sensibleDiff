223654a5e5ddf41b03c2349b01892b1a70766d20
Stefan <you@example.com>
2016-05-03 09:53:58 +0200

    improve robustness

---
+++ test/v2/source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+/* this is a comment which should be
+ * ignored in comparision (#3)
+ */
+
+int a;
+int c;
+
+int main(int argc, char *argv[])
+{
+    int b;
+
+    a = b;
+    // this (#3) comment should be ignored as well
+    b = 1; // this should also be ignored (huh)
+    b = 2;
+    for (int k=0 /* ahallo */; k<b /* this #4 is another one */; k+=1) { /*
+                                                                            weird comment
+                                                                            */ c++;
+        a++;
+    }
+    return 0;
+}
--- v2/helper.cpp
+++
@@ -1,9 +0,0 @@
-void helper(int *something)
-{
-    for (int i = 0; i<maxi; i++) {
-        // one  comment
-        for (int k=0; k<maxk; k++) {
-            int bla = somethin[i*maxk /* something important here*/ maxk]; //oops nothing here
-            donothing(++bla);
-        }
-}
